6268184a98d258b8201f06e41d7e766b6f29c59d
Abhishek Bawiskar <abawiskar@thorntontomasetti.com>
2022-11-05 22:59:57 -0400

    hazard file

--- eqcalc.py
+++ eqcalc.py
@@ -1,2 +1,26 @@
 
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+def get_intensity(lat, long, event):
+    prop_detail = pd.read_json(event["properties.detail"])
+    url = prop_detail["properties"]["products"]["shakemap"][0]["contents"]["download/grid.xml"]["url"]
+    s = requests.get(url).content
+    shakemap_xml = BeautifulSoup(s)
+    
+    gd = shakemap_xml("grid_data")
+    gd_array = gd[0].text.split('\n')
+
+    del gd_array[0]
+    del gd_array[-1]
+
+    min_grid_pt_dist = 100000.00
+    mmi = 0
+
+    for i in range(len(gd_array.index)):
+        
+
+    return 0
+
 
--- hazardapp.py
+++ hazardapp.py
@@ -2,8 +2,11 @@
 
 import json
 import pandas as pd
-
-
+import eqcalc as eqc
+import xml.etree.ElementTree as et
+import requests
+from bs4 import BeautifulSoup
+import eqcalc
 
 def load_data(data):
     json_data = pd.json_normalize(data, record_path =['features'])
@@ -14,6 +17,34 @@ with open('shakemap_query_20221105.json','r') as f:
 
 eqdata = load_data(data)
 
+#TT SF office
+asset1_lat = 37.789480
+asset1_long = -122.394160
+
+intensity_dict = dict()
+
 for i in range(len(eqdata.index)):
+    event = eqdata.iloc[i]
+    event_prop_types = event["properties.types"]
+
+    if "shakemap" in event_prop_types:
+        #get intensity at asset location
+        event_intensity = eqcalc.get_intensity(asset1_lat,asset1_long,event)
+        intensity_dict[event["id"]] = 1.2
+    else:
+        intensity_dict[event["id"]] = -1
+
+
+
+    
+    
+    
+
+    
+    
+
+
+   
+
     print(eqdata.iloc[i]["properties.mag"])
     print(eqdata.iloc[i]["properties.place"])
